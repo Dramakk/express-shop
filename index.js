09cc2b0414e2b01f29fc3ab0ee5c849e722f936c
Dawid <dawidkg9@wp.pl>
2021-01-08 23:20:29 +0100

    Updated product page to fetch data from database. Added variations of the same product on the bottom of the page, below main section

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var serverUtlis = require('./lib/serverUtils');
+var serverUtils = require('./lib/serverUtils');
 var cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
@@ -26,38 +26,81 @@ app.use(session({
     secret: "73D1B1B1BC1DABFB97F216D897B7968E44B06457920F00F2DC6C1ED3BE25AD4C",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge : 1800 }
+    cookie: { maxAge : 1800 },
+    reapInterval: 60,
 }));
 
+var Pool = require('pg').Pool
+var pool = new Pool({
+  user: 'dramak',
+  host: 'localhost',
+  database: 'sklep',
+  password: '',
+  port: 5432,
+})
+
 app.get('/', (req, res) => {
-    serverUtlis.logConnection("Got connection... ", req.connection.remoteAddress);
+    serverUtils.logConnection("Got connection... ", req.connection.remoteAddress);
+
     res.render('home.ejs', {popularProducts: []});
 });
 
 app.get('/list', (req, res) => {
-    serverUtlis.logConnection(`Accessing category: ${req.params.category} `, req.connection.remoteAddress);
+    serverUtils.logConnection(`Accessing category: ${req.params.category} `, req.connection.remoteAddress);
     res.render('products-list.ejs');
 });
 
 app.get('/list/:category', (req, res) => {
-    serverUtlis.logConnection(`Accessing category: ${req.params.category} `, req.connection.remoteAddress);    
-    res.render('products-list.ejs', {categoryName: serverUtlis.convertCategoryName(req.params.category)});
+    serverUtils.logConnection(`Accessing category: ${req.params.category} `, req.connection.remoteAddress);
+    
+    let categoryName = serverUtils.convertCategoryName(req.params.category);
+
+    if(categoryName === -1){
+        res.redirect('/list/');
+    }
+    else{
+        res.render('products-list.ejs', {categoryName: serverUtils.convertCategoryName(req.params.category)});
+    }
 });
 
 app.get('/products/:id', (req, res) => {
-    serverUtlis.logConnection(`Accessing product: ${req.params.id} `, req.connection.remoteAddress);
-    
-    res.render('product-page.ejs', {productData: []});
+    serverUtils.logConnection(`Accessing product: ${req.params.id} `, req.connection.remoteAddress);
+    serverUtils.getProductData(parseInt(req.params.id), pool, (error, result) => {
+        if (error){
+            throw(error);
+        }
+        if(result === -1){
+            res.redirect('/list/');
+        }
+        else{
+            if(result.variations){
+                res.render('product-page.ejs', {
+                    productData: result.productData, 
+                    variations: result.variations,
+                    hasVariations: true,
+                    variationsLength: Object.keys(result.variations).length
+                });
+            }
+            else{
+                res.render('product-page.ejs', {
+                    productData: result.productData, 
+                    variations: [],
+                    hasVariations: false,
+                    variationsLength: 0
+                });
+            }
+        }
+    })
 });
 
 app.get('/login', (req, res) => {
-    serverUtlis.logConnection("Accessing login page... ", req.connection.remoteAddress);
+    serverUtils.logConnection("Accessing login page... ", req.connection.remoteAddress);
     
     res.render('login.ejs');
 });
 
 app.get('/register', (req, res) => {
-    serverUtlis.logConnection("Accessing register page... ", req.connection.remoteAddress);
+    serverUtils.logConnection("Accessing register page... ", req.connection.remoteAddress);
     
     res.render('register.ejs');
 });
